565ce7097073e406116bb15abe12226e92af90d4
Eesa <eesa@chatbase.co>
2025-10-07 19:45:56 +0300

    fix script

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { MinimalNav } from "@/components/minimal-nav"
 import { MinimalHero } from "@/components/minimal-hero"
 import { ProductSection } from "@/components/product-section"
 import { PricingSection } from "@/components/pricing-section"
+import Script from "next/script"
 
 export default function Home() {
   return (
