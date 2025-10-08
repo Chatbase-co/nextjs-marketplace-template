e98c7e93b029580993039e8bededf467e7c2c515
Eesa <eesa@chatbase.co>
2025-10-08 13:52:09 +0300

    set host

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
     <main className="min-h-screen">
 	<Script>
 	{
-		`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="${
-		process.env.NEXT_PUBLIC_CHATBOT_ID
-		}";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`
+		`(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");
+    script.src="${process.env.NEXT_PUBLIC_CHATBASE_HOST || 'https://www.chatbase.co/embed.min.js'}";
+    script.id="${ process.env.NEXT_PUBLIC_CHATBOT_ID}";
+    script.domain="${process.env.NEXT_PUBLIC_CHATBASE_HOST || 'www.chatbase.co'}";
+    document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();`
 	}
 	  </Script>
       <MinimalNav />
